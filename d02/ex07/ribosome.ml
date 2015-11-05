0926a1b3c20be26518ee861cb774053858c923aa
Thomas PAYET <tpayet@e1r9p19.42.fr>
2015-11-05 22:02:39 +0100

    ex07 okay

--- d02/ex07/ribosome.ml
+++ d02/ex07/ribosome.ml
@@ -76,30 +76,32 @@ let complementary_helix h =
 				  						| T -> 'A'
 				  						| C -> 'G'
 				  						| G -> 'C'
-				  						| None -> 'Z'
+				  						| _ -> 'Z'
 				  				end
 				  				::c_h) q
 				| _ -> c_h
 		in list_reverse (loop [] h)
 
-type rna = nucleotide list
+type rna = nucleobase list
 
 let generate_rna h =
 	if h = [] then []
 	else
 		let rec loop rna o_h =
 			match o_h with
-			  t::q ->loop (generate_nucleotide begin
+			  t::q ->loop (begin
 			  					match t.n with
-			  						  A-> 'U'
-			  						| T -> 'A'
-			  						| C -> 'G'
-				  					| G -> 'C'
-				  					| None -> 'Z'
+			  						  A -> U
+			  						| T -> A
+			  						| C -> G
+				  					| G -> C
+				  					| _ -> None
 			  				end
 			  				::rna) q
 			| _ -> rna
 		in list_reverse (loop [] h)
+
+(*****************************************************************************)
 
 type aminoacid = Ala | Arg | Asn | Asp | Cys
 				| Gln | Glu | Gly | His | Ile
@@ -116,13 +118,108 @@ let generate_base_triplets rna =
 			| _ -> t
 	in list_reverse (loop rna [])
 
-let decode_arn rna =
+(* let decode_arn rna =
 	let rec loop t p =
 		match t with
-			  ((U, A, A) | (U, A, G) | (U, G, A))::tail -> list_rev (Stop::p)
-			| ((G, C, A) | (G, C, C) | (G, C, G) | (G, C, U))::tail -> list_rev (Ala::p)
-			| ((A, G, A) | (A, G, G) | (C, G, A) | (C, G, C) | (C, G, G) | (C, G, U))::tail -> list_rev (Arg::p)
-			| ((A, A, C) | (A, A, U))::tail -> list_rev (Asn::p)
+			  ((U, A, A) | (U, A, G) | (U, G, A))::tail -> list_reverse (Stop::p)
+			| ((G, C, A) | (G, C, C) | (G, C, G) | (G, C, U))::tail -> loop t (Ala::p)
+			| ((A, G, A) | (A, G, G) | (C, G, A) | (C, G, C) | (C, G, G) | (C, G, U))::tail -> loop t (Arg::p)
+			| ((A, A, C) | (A, A, U))::tail -> loop t (Asn::p)
+			| ((G, A, C) | (G, A, U))::tail -> loop t (Asp::p)
+    		| ((U, G, C) | (U, G, U))::tail -> loop t (Cys::p)
+    		| ((C, A, A) | (C, A, G))::tail -> loop t (Gln::p)
+    		| ((G, A, A) | (G, A, G))::tail -> loop t (Glu::p)
+    		| ((G, G, A) | (G, G, C) | (G, G, G) | (G, G, U))::tail -> loop t (Gly::p)
+    		| ((C, A, C) | (C, A, U))::tail -> loop t (His::p)
+    		| ((A, U, A) | (A, U, C) | (A, U, U))::tail -> loop t (Ile::p)
+    		| ((C, U, A) | (C, U, C) | (C, U, G) | (C, U, U) | (U, U, A) | (U, U, G))::tail -> loop t (Leu::p)
+		    | ((A, A, A) | (A, A, G))::tail -> loop t (Lys::p)
+		    | ((A, U, G))::t -> loop t (Met::p)
+		    | ((U, U, C) | (U, U, U))::t -> loop t (Phe::p)
+		   	| ((C, C, C) | (C, C, A) | (C, C, G) | (C, C, U))::t -> loop t (Pro::p)
+		    | ((U, C, A) | (U, C, C) | (U, C, G) | (U, C, U) | (A, G, U) | (A, G, C))::t -> loop t (Ser::p)
+		    | ((A, C, A) | (A, C, C) | (A, C, G) | (A, C, U))::t -> loop t (Thr::p)
+		    | ((U, G, G))::t -> loop t (Trp::p)
+		    | ((U, A, C) | (U, A, U))::t -> loop t (Tyr::p)
+		    | ((G, U, A) | (G, U, C) | (G, U, G) | (G, U, U))::t -> loop t (Val::p)
+			| _ -> []
+	in loop (generate_base_triplets rna) [] *)
 
+let decode_arn rna =
+  let rec loop tri pro =
+	match tri with
+	  ((U, A, A) | (U, A, G) | (U, G, A))::t                                     -> list_reverse (Stop::pro)
+    | ((G, C, A) | (G, C, C) | (G, C, G) | (G, C, U))::t                         -> loop t (Ala::pro)
+    | ((A, G, A) | (A, G, G) | (C, G, A) | (C, G, C) | (C, G, G) | (C, G, U))::t -> loop t (Arg::pro)
+    | ((A, A, C) | (A, A, U))::t                                                 -> loop t (Asn::pro)
+    | ((G, A, C) | (G, A, U))::t                                                 -> loop t (Asp::pro)
+    | ((U, G, C) | (U, G, U))::t                                                 -> loop t (Cys::pro)
+    | ((C, A, A) | (C, A, G))::t                                                 -> loop t (Gln::pro)
+    | ((G, A, A) | (G, A, G))::t                                                 -> loop t (Glu::pro)
+    | ((G, G, A) | (G, G, C) | (G, G, G) | (G, G, U))::t                         -> loop t (Gly::pro)
+    | ((C, A, C) | (C, A, U))::t                                                 -> loop t (His::pro)
+    | ((A, U, A) | (A, U, C) | (A, U, U))::t                                     -> loop t (Ile::pro)
+    | ((C, U, A) | (C, U, C) | (C, U, G) | (C, U, U) | (U, U, A) | (U, U, G))::t -> loop t (Leu::pro)
+    | ((A, A, A) | (A, A, G))::t                                                 -> loop t (Lys::pro)
+    | ((A, U, G))::t                                                             -> loop t (Met::pro)
+    | ((U, U, C) | (U, U, U))::t                                                 -> loop t (Phe::pro)
+    | ((C, C, C) | (C, C, A) | (C, C, G) | (C, C, U))::t                         -> loop t (Pro::pro)
+    | ((U, C, A) | (U, C, C) | (U, C, G) | (U, C, U) | (A, G, U) | (A, G, C))::t -> loop t (Ser::pro)
+    | ((A, C, A) | (A, C, C) | (A, C, G) | (A, C, U))::t                         -> loop t (Thr::pro)
+    | ((U, G, G))::t                                                             -> loop t (Trp::pro)
+    | ((U, A, C) | (U, A, U))::t                                                 -> loop t (Tyr::pro)
+    | ((G, U, A) | (G, U, C) | (G, U, G) | (G, U, U))::t                         -> loop t (Val::pro)
+	| _ -> []
+  in loop (generate_base_triplets rna) []
 
+let string_of_protein pro =
+    let rec loop l str =
+      match l with
+        []       -> str
+      | hd :: tl -> loop tl (str ^ begin match hd with
+                                            Ala -> "Alanine"
+										  | Arg -> "Arginine"
+										  | Asn -> "Asparagine"
+										  | Asp -> "Aspartique"
+										  | Cys -> "Cysteine"
+										  | Gln -> "Glutamine"
+										  | Glu -> "Glutamique"
+										  | Gly -> "Glycine"
+										  | His -> "Histidine"
+										  | Ile -> "Isoleucine"
+										  | Leu -> "Leucine"
+										  | Lys -> "Lysine"
+										  | Met -> "Methionine"
+										  | Phe -> "Phenylalanine"
+										  | Pro -> "Proline"
+										  | Ser -> "Serine"
+										  | Thr -> "Threonine"
+										  | Trp -> "Tryptophane"
+										  | Tyr -> "Tyrosine"
+										  | Val -> "Valine"
+										  | Stop -> "EOP"
+                                    end ^ "; ")
+    in loop pro ""
 
+(*****************************************************************************)
+
+let() =
+	Random.self_init ();
+	let string_of_rna rna =
+		let rec loop r str =
+	  	match r with
+		[]       -> str
+	  | hd :: tl -> loop tl (str ^ begin match hd with
+											A    -> "A"
+										  | T    -> "T"
+										  | C    -> "C"
+										  | G    -> "G"
+										  | U    -> "U"
+										  | _ -> "None"
+									end ^ "; ")	  
+	in loop rna ""
+  in let h = generate_helix 1000 in
+	 print_endline ("Testing "^(string_of_int 1000)^":");
+	 print_endline ("\nHelix :\n"^(helix_to_string h)); 
+	 print_endline ("\nRNA :\n"^(string_of_rna (generate_rna h)));
+	 print_endline ("\nProtein :\n"^(string_of_protein (decode_arn (generate_rna h))))

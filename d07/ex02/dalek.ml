4702fd8d71584aaf317b057fafc12294d5e6e9aa
Thomas PAYET <tpayet@e2r8p1.42.fr>
2015-11-12 19:18:24 +0100

    123

--- d07/ex02/dalek.ml
+++ d07/ex02/dalek.ml
@@ -1,11 +1,23 @@
-class dalek =
+class dalek ?(hp=100) =
 	object (self)
 
+		val _hp = hp
 
 
-		val _name:string
+		val _name:string = Dalek 	^ String.make 1 (char_of_int ((Random.int 26) + 65))
+									^ String.make 1 (char_of_int ((Random.int 26) + 97))
+									^ String.make 1 (char_of_int ((Random.int 26) + 97))
 
-		method set_name =
-			char
+		val mutable _shield = true
 
+		method talk =
+			let aux i =
+				match i with 
+				  0 -> "Explain! Explain!"
+				| 1 -> "Exterminate! Exterminate!"
+				| 2 -> "I obey!"
+				| 3 -> "You are the Doctor! You are the enemy of the Daleks!"
+			in print_endline (aux Random.int 3)
+
+		method exterminate (p:People.people) =
 	end
